b813405ee4847da9ba9cba6cb24b62581e9f0e5e
Abe Tomoaki <abe@enzou.tokyo>
2025-11-03 12:05:21 +0900

    Add benchmark scripts (#1)

---
+++ benchmarks/flag.js
@@ -0,0 +1,41 @@
+const n = 100_000
+
+console.time('no reassignment IIFE')
+for (let t = 0; t < n; t++) {
+  const flag = (() => {
+    if (t % 2 === 0) {
+      return true
+    }
+    if (t % 7 === 0) {
+      return true
+    }
+    return false
+  })()
+}
+console.timeEnd('no reassignment IIFE')
+
+console.time('no reassignment func call')
+const checkValue = (num) => {
+  if (num % 2 === 0) {
+    return true
+  }
+  if (num % 7 === 0) {
+    return true
+  }
+  return false
+}
+for (let t = 0; t < n; t++) {
+  const flag = checkValue(t)
+}
+console.timeEnd('no reassignment func call')
+
+console.time('reassignment')
+for (let t = 0; t < n; t++) {
+  let flag = false
+  if (t % 2 === 0) {
+    flag = true
+  } else if (t % 7 === 0) {
+    flag = true
+  }
+}
+console.timeEnd('reassignment')
